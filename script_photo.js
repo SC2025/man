dd75cf8605287996cdbc418fb45f4fcc23f6b1cc
SC2025 <168258416+SC2025@users.noreply.github.com>
2024-07-24 11:39:33 +0530

    Update script_photo.js

--- script_photo.js
+++ script_photo.js
@@ -1,26 +1,19 @@
-// product photo change function 
-
-
-            
-            
 var slideM13Index = 0;
-showSlidesM13(); 
+showSlidesM13();
+
 function showSlidesM13() {
-let i;
-let slidesM13 = document.getElementsByClassName("mySlidesM13");
-let dotsM13 = document.getElementsByClassName("dotM13");
-for (i = 0; i < slidesM13.length; i++) {
-slidesM13[i].style.display = "none";  
+  let i;
+  let slidesM13 = document.getElementsByClassName("mySlidesM13");
+  let dotsM13 = document.getElementsByClassName("dotM13");
+  for (i = 0; i < slidesM13.length; i++) {
+    slidesM13[i].style.display = "none";  
+  }
+  slideM13Index++;
+  if (slideM13Index > slidesM13.length) {slideM13Index = 1}    
+  for (i = 0; i < dotsM13.length; i++) {
+    dotsM13[i].className = dotsM13[i].className.replace(" active", "");
+  }
+  slidesM13[slideM13Index-1].style.display = "block";  
+  dotsM13[slideM13Index-1].className += " active";
+  setTimeout(showSlidesM13, 2000); // Change image every 2 seconds
 }
-slideM13Index++;
-if (slideM13Index > slidesM13.length) {slideM13Index = 1}    
-for (i = 0; i < dotsM13.length; i++) {
-dotsM13[i].className = dotsM13[i].className.replace("active", "");
-}
-slidesM13[slideM13Index-1].style.display = "block";  
-dotsM13[slideM13Index-1].className += "active";
-setTimeout(showSlidesM13, 2000); // Change image every 2 seconds
-}
-
-     
-
